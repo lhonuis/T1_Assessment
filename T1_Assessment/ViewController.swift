06662229677f85d87036e5470b501d60a488016c
lhonuis <louis1991@hotmail.com>
2019-09-23 12:53:28 +0800

    Project setup

---
+++ T1_Assessment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  T1_Assessment
+//
+//  Created by Louis Hon on 23/9/2019.
+//  Copyright © 2019 Louis Hon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
