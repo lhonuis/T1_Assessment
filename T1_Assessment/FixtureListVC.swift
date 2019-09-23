4b3b69a1fde5e470895d1a790256e6da61ecce92
lhonuis <louis1991@hotmail.com>
2019-09-23 16:14:54 +0800

    Added switches and linked them with API

--- T1_Assessment/FixtureListVC.swift
+++ T1_Assessment/FixtureListVC.swift
@@ -31,6 +31,42 @@ class FixtureListVC: UIViewController, UITableViewDataSource {
         
         cell.fixtureTitleLbl.text = roomInfo[roomNumSelected].fixtureNames[indexPath.row]
         
+        if roomInfo[roomNumSelected].status[indexPath.row] {
+            cell.fixtureSwitch.isOn = true
+        } else {
+            cell.fixtureSwitch.isOn = false
+        }
+        
+        cell.fixtureSwitch.tag = indexPath.row
+        cell.fixtureSwitch.addTarget(self, action:  #selector(toggle(_:)), for: .valueChanged)
+        
         return cell
+    }
+    
+    @objc func toggle(_ sender: UISwitch) {
+        
+        if sender.isOn {
+            toggleFixture(index: sender.tag, status: "on")
+            roomInfo[roomNumSelected].status[sender.tag] = true
+        } else {
+            toggleFixture(index: sender.tag, status: "off")
+            roomInfo[roomNumSelected].status[sender.tag] = false
+        }
+    }
+    
+    func toggleFixture(index: Int, status: String) {
+        
+        let url = URL(string: "https://private-anon-c8ecc4b87c-house4591.apiary-mock.com/\(roomInfo[roomNumSelected].roomName)/\(roomInfo[roomNumSelected].fixtureNames[index])/\(status)")!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let response = response {
+                print(response)
+                if let data = data, let body = String(data: data, encoding: .utf8) {
+                    print(body)
+                }
+            } else {
+                print(error ?? "Unknown error")
+            }
+            }.resume()
     }
 }
